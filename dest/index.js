e551150ed282eac04721d516ec8feab5d7c0cf0f
Tuhin Samanta <tuhinsamantastudent@gmail.com>
2022-12-20 12:22:55 +0530

    Init

---
+++ dest/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
+const ctx = canvas.getContext("2d");
+canvas.height = 500;
+canvas.width = 500;
+canvas.style.backgroundColor = "#140E4D";
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+const canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
+const ctx = canvas.getContext("2d");
+
+canvas.height = 500;
+canvas.width = 500;
+
+canvas.style.backgroundColor = "#140E4D";
